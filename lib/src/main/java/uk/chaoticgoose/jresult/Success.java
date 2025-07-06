733c315ea68fbe8ea17a2a62f1a7ee27e8ef8e03
Josh Jones <ohblonddev@gmail.com>
2025-07-06 16:25:36 +0100

    Remove unnecessary inheritance instance

--- lib/src/main/java/uk/chaoticgoose/jresult/Success.java
+++ lib/src/main/java/uk/chaoticgoose/jresult/Success.java
@@ -3,7 +3,7 @@ package uk.chaoticgoose.jresult;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
-public final class Success<T, E> extends BaseSuccess<T, E> implements BaseResult<T, E>, Result<T, E> {
+public final class Success<T, E> extends BaseSuccess<T, E> implements Result<T, E> {
     Success(T value) {
         super(value);
     }
